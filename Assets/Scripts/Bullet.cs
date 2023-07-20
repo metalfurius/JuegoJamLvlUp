1f5ffd82475b651c3b0fc5fd33c1907321c6672d
metalfurius <hackero1932@gmail.com>
2023-07-20 17:51:03 +0200

    Missile Turret+effect

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ public class Bullet : MonoBehaviour
 {
     private Transform target;
     public float speed = 70f;
+    public float explosionRadius=0f;
     public GameObject impactEffect;
     public void Seek(Transform _target)
     {
@@ -27,13 +28,35 @@ public class Bullet : MonoBehaviour
             return;
         }
         transform.Translate(dir.normalized*distanceThisFrame,Space.World);
+        transform.LookAt(target);
     }
 
     private void HitTarget()
     {
         GameObject effectIns=(GameObject)Instantiate(impactEffect,transform.position,transform.rotation);
-        Destroy(effectIns,2f);
+        Destroy(effectIns,4f);
+        if (explosionRadius>0f){
+            Explode();
+        }
+        else{
+            Damage(target);
+        }
         Destroy(gameObject);
         Destroy(target.gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position,explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if(collider.tag=="Enemy"){
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage (Transform enemy){
+        Destroy(enemy.gameObject);
     }
 }
